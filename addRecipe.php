6bc486b99650dc85b57ba7d4aaa27372d7db8eb1
Durso <dursozd@gmail.com>
2021-06-20 22:21:24 -0400

    burn doesnt hurt any more so i finished the add new recipe functionality!

--- addRecipe.php
+++ addRecipe.php
@@ -14,13 +14,15 @@
 </head>
 <body>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<form action="upload.php" method="POST" enctype="multipart/form-data">
 <label for="title">Title:</label>
 <input type="text" id="title" name="title">
 <br/>
-<input type="file" name="img"><br/>
+<label for="ingredients">Ingredients</label><br/>
 <textarea name="ingredients" rows="15" cols="30"></textarea><br/>
+<label for="instructions">Instructions</label><br/>
 <textarea name="instructions" rows="15" cols="30"></textarea><br/>
+<label for="description">Description</label><br/>
 <textarea name="description" rows="15" cols="30"></textarea><br/>
 
 <button type="submit" value="submit" name="submit">SUBMIT</button>
--- upload.php
+++ upload.php
@@ -1,23 +1,24 @@
 <?php
 require 'connect-to-db.php';
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
 
-if($conn === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
+$title = $_POST['title'];
+$ingredients = $_POST['ingredients'];
+$instructions = $_POST['instructions'];
+$description = $_POST['description'];
 
-$title = $_POST["title"];
-$img = $_POST["img"];
-$ingredients = $_POST["ingredients"];
-$instructions = $_POST["instructions"];
-$description = $_POST["description"];
+$sql="INSERT INTO recipes (title, ingredients, instructions, description) VALUES ('$title', '$ingredients', '$instructions', '$description')";
 
-$sql = "INSERT INTO recipes (title, photo, ingredients, instructions, description) VALUES ($title, $img, $ingredients, $instructions, $description)";
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
 
-if(mysqli_query($conn, $sql)){
-    echo "Records inserted successfully.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
+  mysqli_close($conn);
+ 
 /*
 $target_dir = "recipes/";
 $target_file = $target_dir . basename($_FILES[$img]["name"]);
@@ -30,4 +31,4 @@ https://www.tutorialrepublic.com/php-tutorial/php-mysql-insert-query.php
 https://www.geeksforgeeks.org/how-to-insert-form-data-into-database-using-php/
 https://makitweb.com/make-destination-directory-on-file-upload-with-php/
 */
-
+ ?>
